0575cb6b636b185b0885a6a18b1620663679b0a1
tobyestipona <toby.estipona@gmail.com>
2024-03-24 09:30:45 -0500

    font change

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Question1 from "./components/Question1";
