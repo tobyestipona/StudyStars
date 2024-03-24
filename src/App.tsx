d7aa9d680fba807bb9e137e4bedaf5c59e6eb14b
JaenaO <jaenakboro@gmail.com>
2024-03-24 01:43:16 -0500

    change importing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Question1 from "./components/Question1";
 import Question2 from "./components/Question2";
 import Question3 from "./components/Question3";
 import Question4 from "./components/Question4";
-import Question5 from "./components/Question4";
+import Question5 from "./components/Question5";
 import Question6 from "./components/Question6";
 import Question7 from "./components/Question7";
 import Question8 from "./components/Question8";
